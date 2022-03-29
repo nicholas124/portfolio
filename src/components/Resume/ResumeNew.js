080c04d69a8043aea0df5751b0a09aa9e7e4276d
Nicholas Muchelemba <nmuchelemba@fqml.com>
2022-03-29 13:28:01 +0200

    First Commit

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Nicholas_Muchelemba_Resume.pdf";
+  "https://raw.githubusercontent.com/nicholas124/portfolio/master/src/Assets/Nicholas_Muchelemba_Resume.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
